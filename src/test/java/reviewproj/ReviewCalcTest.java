1213855ab833d56e688134464a22d10e510fac3a
Seong Gwon Ju <jsg6780@naver.com>
2016-04-25 11:18:07 +0900

    뺼셈기능 추가

--- src/test/java/reviewproj/ReviewCalcTest.java
+++ src/test/java/reviewproj/ReviewCalcTest.java
@@ -10,7 +10,14 @@ public class ReviewCalcTest {
 	public void testAdd() {
 		CalcReview r = new CalcReview();
 		r.add(10,  20);
-		assertEquals(30, r.getResult());	
+		assertEquals(30, r.getResult());
+	}
+		
+	@Test
+	public void testSub() {
+		CalcReview r = new CalcReview();
+		r.sub(10,  20);
+		assertEquals(30, r.getResult());
 	}
 
 }
